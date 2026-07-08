48dcda4651f1f66088c7b695c854c5b25e2b1aef
0xdaksh-12 <0xdaksh.12@gmail.com>
2026-07-08 20:33:01 +0530

    feat(client): integrate typed-css-modules for CSS module type safety

---
+++ client/src/pages/Home.module.css.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly "container": string;
+  readonly "dot": string;
+  readonly "dotGreen": string;
+  readonly "dotGrey": string;
+  readonly "dotRed": string;
+  readonly "pulse-green": string;
+  readonly "pulse-red": string;
+  readonly "statusContainer": string;
+  readonly "statusText": string;
+  readonly "title": string;
+};
+export = styles;
+
